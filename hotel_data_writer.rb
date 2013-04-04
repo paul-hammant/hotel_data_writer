69cbc8489b4e529c6e2127be4fbc678be713ff82
Andrew Dean <ndrwdn@gmail.com>
2013-04-03 20:54:03 -0500

    Adding json file handler and test data file.

--- hotel_data_writer.rb
+++ hotel_data_writer.rb
@@ -10,6 +10,14 @@ post '/createOrLocate' do
     redirect "/" + params[:state] + "/" + params[:zip] + "/" + params[:city] + "/" + params[:name]
 end
 
+get '/:state/:zip/:city/:name.json' do |state, zip, city, name|
+    content_type 'application/json', :charset => 'utf-8'
+
+    hotelPath = "./#{state}/#{zip}/#{city}/#{name}.json"
+    hotelFile = File.open(hotelPath, "r")
+    hotelFile.read
+end
+
 get '/:state/:zip/:city/:name' do |state, zip, city, name|
     send_file 'edit.html'
 end
