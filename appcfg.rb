c5b6d3773217e0c3c6e75349968c68412369b9a4
Andrew Dean <ndrwdn@gmail.com>
2013-04-03 19:13:30 -0500

    Zaki|Andrew Post handler for creation of key values.

--- appcfg.rb
+++ appcfg.rb
@@ -5,3 +5,7 @@ set :public_folder, File.dirname(__FILE__)
 get '/' do
     redirect "index.html"
 end
+
+post '/createOrLocate' do
+    redirect "/" + params[:state] + "/" + params[:zip] + "/" + params[:city] + "/" + params[:name]
+end
