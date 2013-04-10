405ab3bdc09469f5f7b3080f00516fc94dcac9f9
Andrew Dean <ndrwdn@gmail.com>
2013-04-10 17:49:59 -0500

    Normalize use of quotes.

--- hotel_data_writer.rb
+++ hotel_data_writer.rb
@@ -3,18 +3,18 @@ require 'sinatra'
 set :public_folder, File.dirname(__FILE__)
 
 get '/' do
-    redirect "index.html"
+    redirect 'index.html'
 end
 
 post '/createOrLocate' do
-    redirect "/" + params[:state] + "/" + params[:zip] + "/" + params[:city] + "/" + params[:name]
+    redirect '/' + params[:state] + '/' + params[:zip] + '/' + params[:city] + '/' + params[:name]
 end
 
 get '/:state/:zip/:city/:name.json' do |state, zip, city, name|
     content_type 'application/json', :charset => 'utf-8'
 
     hotelPath = "./#{state}/#{zip}/#{city}/#{name}.json"
-    hotelFile = File.open(hotelPath, "r")
+    hotelFile = File.open(hotelPath, 'r')
     hotelFile.read
 end
 
